717106367a1c0736de49b5821cbae505a444d40d
Jonathan Dorety <kyullog@gmail.com>
2019-03-26 23:45:25 -0400

    Add register API endpoint w/ password saved as hash

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const helmet = require("helmet");
+const bcrypt = require("bcryptjs");
+const db = require("./data/helpers/users.js");
 
 const server = express();
 server.use(helmet(), express.json());
+
+server.post("/api/register", async (req, res) => {
+  try {
+    const user = req.body;
+    if (user.name && user.password) {
+      const hashword = bcrypt.hashSync(user.password, 10);
+      user.password = hashword;
+
+      const added = await db.addUser(user);
+      console.log(added);
+      res.status(201).json(added);
+    } else {
+      res.status(400).json({ message: "Please provide credentials" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = server;
