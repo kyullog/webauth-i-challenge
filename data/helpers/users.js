64081c9145261d9792310265c8b5039187222758
Jonathan Dorety <kyullog@gmail.com>
2019-03-27 21:23:55 -0400

    Add findUser helper method

--- data/helpers/users.js
+++ data/helpers/users.js
@@ -1,9 +1,14 @@
 const db = require("../dbConfig.js");
 
 module.exports = {
-  addUser
+  addUser,
+  findUser
 };
 
 function addUser(user) {
   return db("users").insert(user);
+}
+
+function findUser(user) {
+  return db("users").where({ user });
 }
