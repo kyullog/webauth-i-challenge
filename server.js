0f04de0e8d1490be1e80968085ec7063b2021bdb
Jonathan Dorety <kyullog@gmail.com>
2019-03-27 21:53:35 -0400

    Create login API endpoint

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const db = require("./data/helpers/users.js");
 const server = express();
 server.use(helmet(), express.json());
 
+function restricted(req, res, next) {}
+
 server.post("/api/register", async (req, res) => {
   try {
     const user = req.body;
@@ -22,6 +24,31 @@ server.post("/api/register", async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
+  }
+});
+
+server.post("/api/login", async (req, res) => {
+  try {
+    const user = req.body;
+    if (user.name && user.password) {
+      const found = await db.findUser(user);
+      if (found) {
+        const match = bcrypt.compareSync(user.password, found.password);
+
+        if (match) {
+          res.status(200).json({ message: "Logged in!" });
+        } else {
+          res.status(400).json({ message: "None shall pass!" });
+        }
+      } else {
+        res.status(400).json({ message: "Please provide correct credentials" });
+      }
+    } else {
+      res.status(400).json({ message: "Please provide valid credentials" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status({ error: "There was a problem logging you in" });
   }
 });
 
