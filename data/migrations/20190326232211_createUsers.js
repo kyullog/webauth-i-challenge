88887d302884c79635aecfe389fbbad60b5614b0
Jonathan Dorety <kyullog@gmail.com>
2019-03-27 21:52:30 -0400

    Simplify users table schema name

--- data/migrations/20190326232211_createUsers.js
+++ data/migrations/20190326232211_createUsers.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("users", tbl => {
     tbl.increments();
     tbl
-      .string("username")
+      .string("name")
       .notNullable()
       .unique();
     tbl.string("password").notNullable();
