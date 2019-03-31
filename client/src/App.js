9e708112f3593dcd50fb917e6344e56c2ef6c483
Jonathan Dorety <kyullog@gmail.com>
2019-03-31 11:02:06 -0400

    Add react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import LoginWindow from "./Login/LoginWindow";
+import { Route } from "react-router-dom";
+import LoginWindow from "./Components/Login/LoginWindow";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LoginWindow />
+        <Route path="/login" exact component={LoginWindow} />
       </div>
     );
   }
